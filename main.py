8673ccaff1dc54e450d4c393b6d749855280a4a6
Juwek <andreygorbunov000@gmail.com>
2024-12-08 14:09:25 +0700

    add ui class

--- main.py
+++ main.py
@@ -3,69 +3,29 @@ import io
 
 from PyQt6 import uic, QtCore
 from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QPainter
+from PyQt6.QtGui import QPainter, QColor
 from PyQt6.QtWidgets import QWidget, QApplication, QPushButton
 
 from random import choice, random
-
-template = '''<?xml version="1.0" encoding="UTF-8"?>
-<ui version="4.0">
- <class>MainWindow</class>
- <widget class="QWidget" name="MainWindow">
-  <property name="geometry">
-   <rect>
-    <x>0</x>
-    <y>0</y>
-    <width>800</width>
-    <height>600</height>
-   </rect>
-  </property>
-  <property name="windowTitle">
-   <string>MainWindow</string>
-  </property>
-  <widget class="QWidget" name="centralwidget">
-   <widget class="QPushButton" name="button">
-    <property name="geometry">
-     <rect>
-      <x>350</x>
-      <y>410</y>
-      <width>131</width>
-      <height>51</height>
-     </rect>
-    </property>
-    <property name="text">
-     <string>Тык</string>
-    </property>
-   </widget>
-  </widget>
-  <widget class="QMenuBar" name="menubar">
-   <property name="geometry">
-    <rect>
-     <x>0</x>
-     <y>0</y>
-     <width>800</width>
-     <height>21</height>
-    </rect>
-   </property>
-  </widget>
-  <widget class="QStatusBar" name="statusbar"/>
- </widget>
- <resources/>
- <connections/>
-</ui>
-'''
 
 
 class Example(QWidget):
     def __init__(self):
         super().__init__()
+        self.setGeometry(300, 300, 800, 600)
         self.initUI()
 
     def initUI(self):
-        f = io.StringIO(template)
-        uic.loadUi(f, self)
         self.setWindowTitle('Рисование')
-        self.button: QPushButton = self.button
+        ui = Ui(self)
+
+
+class Ui(QWidget):
+    def __init__(self, window):
+        super().__init__(window)
+        self.button = QPushButton('Тык', self)
+        self.button.move(350, 400)
+        self.button.resize(100, 40)
         self.button.clicked.connect(self.paint_circle)
         self.paint = False
 
@@ -73,7 +33,8 @@ class Example(QWidget):
         super().paintEvent(event)
         if self.paint:
             qp = QPainter(self)
-            qp.setBrush(Qt.GlobalColor.yellow)
+            r, g, b = choice(range(256)), choice(range(256)), choice(range(256))
+            qp.setBrush(QColor(r, g, b))
             x = choice(range(801))
             y = choice(range(601))
             d = choice(range(301))
